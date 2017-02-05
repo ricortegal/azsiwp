20662623c09141333f336816961296872b42fc77
Ricardo Ortega Galiana <ricortegal@gmail.com>
2017-02-05 11:15:24 +0100

    commit inicial

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package azsiwp
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'azsiwp' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'azsiwp' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'azsiwp' ), 'azsiwp', '<a href="https://automattic.com/" rel="designer">Ricardo Ortega</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
